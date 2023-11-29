6612a601f60d78ddfa6fd0f959954bbb12256bf1
CarlosGuzman01 <carlosguzmantejeda13@gmail.com>
2023-11-29 09:29:04 -0500

    set up click listener to update the beer color on textView

--- app/src/main/java/com/example/beeradviser/MainActivity.kt
+++ app/src/main/java/com/example/beeradviser/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.beeradviser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val findBeer = findViewById<Button>(R.id.find_beer)
+
+
+
+        findBeer.setOnClickListener {
+            val beerColor = findViewById<Spinner>(R.id.beer_color)
+            val color = beerColor.selectedItem.toString()
+
+            val brands = findViewById<TextView>(R.id.brands)
+            brands.text = color
+
+
+
+        }
     }
 }
